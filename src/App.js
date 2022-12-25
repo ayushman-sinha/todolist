e6ac3dd2cb42e1894ed84ec4e10e59bdbeb0f94e
Ayushman Sinha <ayushman.sinhaa@gmail.com>
2022-12-26 02:22:13 +0530

    v 1.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/body.css';
 import Blob from './Blob';
 import ListItem from './ListItem';
 function App() {
+  const [listItemArray, setListItemArray] = useState([])
+  const [title, setTitle] = useState('')
+
+  const deleteItem = (id) => {
+    const tmp = listItemArray.filter((item)=>item.id!=id)
+    console.log(tmp)
+    setListItemArray(tmp)
+  }
+  const handleAdd = (e) => {
+    const tmp= <ListItem title={title} key={listItemArray.length+1} id={listItemArray.length+1} deleteItem={deleteItem}/>
+    setListItemArray([...listItemArray, tmp])
+    setTitle('')
+  }
+  
+
   return (
     <div>
       <div class='main_container'>
         <div class='header'>
           <h1>Todo List</h1>      
           <div class = 'add_button' >
-            <input type="text" placeholder="Add a todo" />
-            <button>Add</button>        
+            <input type="text" placeholder="Add a todo" value={title} onChange={(e)=>setTitle(e.target.value)}/>
+            <button onClick={handleAdd}>Add</button>        
           </div>
         </div>
-        <ListItem /> 
-        <ListItem />
-        <ListItem />
-        <ListItem />
+        {listItemArray.map((item,key) =>   
+            item
+        )}
 
       
       </div>
--- src/ListItem.js
+++ src/ListItem.js
@@ -1,35 +1,39 @@
 import { useState } from 'react'
 import './css/list_item.css'
-const ListItem = () => {
+const ListItem = (props) => {
     const [showMore, setShowMore] = useState(false); // To display more text 
     const [edit, setEdit] = useState(false); // To edit the text
     const [text, setText] = useState(''); // To store the text
-    const [textEdit, setTextEdit] = useState('Loewm'); // To store the final text
+    const [textEdit, setTextEdit] = useState(props.title); // To store the final text
     const [moreText, setMoreText] = useState(''); // To store the more text
     const [moreTextEdit, setMoreTextEdit] = useState(''); // To store the final more text
-
-    const handleEdit = (e) => {
+    const handleEdit = (e) => {  
+        console.log('edit')     
         setEdit(!edit);
         setShowMore(true);
     }
-    const handleTextEdit = (e) => {
-        
-        setText(e.target.value);
+    const handleTextEdit = (e) => {      
        
+        setText(e.target.value);       
     }
     const saveTextEdit = (e) => {
-        setTextEdit(text);
-        setEdit(!edit);
-        setShowMore(false);
-        setMoreTextEdit(moreText);
+        setTextEdit(text);// To store the final text
+        setEdit(!edit);// Indicate Edit is off
+        setShowMore(false); // Hide more text
+        setMoreTextEdit(moreText);// To store the final more text
     }
     const cancelTextEdit = (e) => {
         setEdit(!edit);
         setShowMore(false);
     }
+    const handleDelete = (e) => {
+        // To delete the item on click of delete button       
+        props.deleteItem(props.id);
+    }
   return (
     <div class = 'todo_list_item'>
-        <div class='normal_text'>
+        
+        <div class='normal_text'>            
             {edit ? 
             <div class= 'editOn'>
                 <input  class = 'item_edit_title' type="text" value={text} onChange={handleTextEdit} />
@@ -41,11 +45,10 @@ const ListItem = () => {
                 <div class = 'item_title'>{textEdit}</div>
                 <div class='edit_delete'>            
                     <button class = 'item_button' onClick={handleEdit} >Edit</button>
-                    <button class = 'item_button'>Delete</button>
+                    <button class = 'item_button' onClick={(e)=>{handleDelete()}}>Delete</button>
                     <button class = 'item_button' onClick={(e)=>setShowMore(!showMore)} >More</button>
                 </div>
             </div>
-            
             }        
             
         </div>
